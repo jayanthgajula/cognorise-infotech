02cd163419f358daa4a8d5276abc27f21244f231
jayanthgajula <158825851+jayanthgajula@users.noreply.github.com>
2024-02-18 19:53:50 +0530

    Create calculatorApp.py

---
+++ calculatorApp.py
@@ -0,0 +1,12 @@
+x=int(input())
+operator=input()
+y=int(input())
+if operator=='+':
+    print('addition',float(x+y))
+if operator=='-':    
+    print('substraction',int(x-y))
+if operator=='/':
+    print('division',int(x/y))
+if operator=='*':
+    print('multiplication',int(x*y))
+    
